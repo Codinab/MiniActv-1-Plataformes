276290b1ee1983a584fba1b089a9adffef34d1f9
Codinab <alex.codinab@gmail.com>
2024-02-15 18:47:38 +0100

    Bones practiques Apartat A -> 1,2 i 3

--- app/src/main/java/com/example/testaccelerometrekotlin/TestAccelerometreActivity.kt
+++ app/src/main/java/com/example/testaccelerometrekotlin/TestAccelerometreActivity.kt
@@ -10,10 +10,12 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.testaccelerometrekotlin.databinding.MainBinding
 
 
-class TestAccelerometreActivity : Activity(), SensorEventListener {
+class TestAccelerometreActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private var color = false
@@ -21,6 +23,15 @@ class TestAccelerometreActivity : Activity(), SensorEventListener {
     private var lastUpdate: Long = 0
 
     private lateinit var binding: MainBinding
+
+    private val greenBackgroundColor by lazy {
+        ContextCompat.getColor(this, R.color.view_background_green)
+    }
+
+    private val redBackgroundColor by lazy {
+        ContextCompat.getColor(this, R.color.view_background_red)
+    }
+
 
     /** Called when the activity is first created.  */
 
@@ -38,6 +49,8 @@ class TestAccelerometreActivity : Activity(), SensorEventListener {
         )
         // register this class as a listener for the accelerometer sensor
         lastUpdate = System.currentTimeMillis()
+
+        println("Test")
     }
 
     override fun onSensorChanged(event: SensorEvent) {
@@ -46,23 +59,30 @@ class TestAccelerometreActivity : Activity(), SensorEventListener {
 
     private fun getAccelerometer(event: SensorEvent) {
         val values = event.values
+        if (event.values.size < 3) return;
+
         // Movement
         val x = values[0]
         val y = values[1]
         val z = values[2]
-        val accelerationSquareRoot = (x * x + y * y + z * z
+        val accelerationSquareRoot = ((x * x + y * y + z * z)
                 / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH))
+
+
         val actualTime = System.currentTimeMillis()
-        if (accelerationSquareRoot >= 200) {
-            if (actualTime - lastUpdate < 1000) {
+
+        println("Acceleration: $accelerationSquareRoot")
+
+        if (accelerationSquareRoot >= MIN_ACCELERATION) {
+            if (actualTime - lastUpdate < ACCELEROMETER_UPDATE_TIME) {
                 return
             }
             lastUpdate = actualTime
             Toast.makeText(this, R.string.shuffed, Toast.LENGTH_SHORT).show()
             if (color) {
-                view.setBackgroundColor(Color.GREEN)
+                view.setBackgroundColor(greenBackgroundColor)
             } else {
-                view.setBackgroundColor(Color.RED)
+                view.setBackgroundColor(redBackgroundColor)
             }
             color = !color
         }
@@ -76,5 +96,21 @@ class TestAccelerometreActivity : Activity(), SensorEventListener {
         // unregister listener
         super.onPause()
         sensorManager.unregisterListener(this)
+    }
+
+    override fun onResume() {
+        // register OnResume
+        super.onResume()
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+
+    companion object {
+        private const val MIN_ACCELERATION = 1.1f
+        private const val ACCELEROMETER_UPDATE_TIME = 1000
     }
 }
